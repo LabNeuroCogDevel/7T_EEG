fb7e128c3d741436340975e1918d1fce8f525c43
shanemckeon <sdm63@pitt.edu>
2023-11-06 12:40:35 -0500

    adding SNR rscripts

---
+++ Rscripts/SNR/CombineSubjectDataframes.R
@@ -0,0 +1,46 @@
+# Libraries ----
+
+library(LNCDR)
+library(data.table)
+library(dplyr)
+library(factoextra)
+library(ggplot2)
+library(e1071)
+library(caret)
+attach(mtcars)
+library(grid)
+library(gridExtra)
+library(plotrix)
+library(mgcv)
+library(readxl)
+library(lme4)
+library(lubridate)
+library(checkmate)
+library(lmerTest)
+library(tidyr)
+library(jtools)
+
+# Initialize directory and empty dataframes ----
+
+# Set your working directory to the folder containing your CSV files
+setwd("/Volumes/Hera/Projects/7TBrainMech/scripts/eeg/Shane/Results/SNR/individual_subject_files/")
+
+# List all CSV files in the directory
+csv_files <- list.files(pattern = ".csv")
+
+# Initialize an empty data frame to store the combined data
+combined_data <- data.frame()
+
+# Loop through each CSV file, read it, and combine it ----
+for (file in csv_files) {
+  data <- read.csv(file, header = TRUE)  # Change header argument if needed
+  combined_data <- rbind(combined_data, data)
+}
+
+# Prep the file for merge 7t eeg ----
+
+# rename channel to urchan to merge with channel labels and coordinates 
+names(combined_data)[names(combined_data) == "Channel"] <- "urchan"
+
+
+write.csv(combined_data, file = '/Volumes/Hera/Projects/7TBrainMech/scripts/eeg/Shane/Results/SNR/allSubjectsSNR.csv', row.names = F)
