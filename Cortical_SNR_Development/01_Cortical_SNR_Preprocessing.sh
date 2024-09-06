d0a453a76142187497e1cae9a960c77f8faa5833
WillForan <willforan@gmail.com>
2024-09-06 16:18:09 -0400

    move Cortical_SNR_Development into its own repo

--- Cortical_SNR_Development/01_Cortical_SNR_Preprocessing.sh
+++
@@ -1,9 +0,0 @@
-# Preprocessing all auditory state data for the cortical SNR development project
-# Pull in raw data from hera('Raw/EEG/7TBrainMech')
-# run_preprocessing_pipeline.m (/Volumes/Hera/Projects/7TBrainMech/scripts/eeg/Shane/Preprocessing_Functions)
-# Set task as 'SNR'
-# This only downsamples the data from 1024 Hz to 512 Hz (as opposed to other studies that are downsampled to 150 Hz)
-# Fully preprocessed data will be in Hera/Projects/7TBrainMech/scripts/eeg/Shane/preprocessed_data/SNR/AfterWhole/ICAwholeClean_homogenize
-
-matlab -nodesktop -r "addpath(genpath('../Preprocessing_Functions/')); run_preprocessing_pipeline('SNR')"
-
--- Cortical_SNR_Development/03_CombineSubDataframes.R
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env Rscript
-
-source("code/CombineSubjectDataFrames.R")
-
-combineSubjectDataframes(20)
